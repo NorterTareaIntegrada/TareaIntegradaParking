7cd8d7c613f454390fe2ca0de1f9fa7b3d40ef1d
admin <admin@Win7_1TB>
2017-05-18 11:19:11 +0200

    A servicio le he creado su (constructor,gett,sett) y a la bd_conector su buscar por nombre.

--- TareaIntegradaParking/src/BBDD/BD_Servicio.java
+++ TareaIntegradaParking/src/BBDD/BD_Servicio.java
@@ -1,5 +1,42 @@
 package BBDD;
 
-public class BD_Servicio {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.Vector;
+import items.Servicios;
+
+
+public class BD_Servicio extends BBDD_Connector {
+
+	private static Statement s;	
+	private static ResultSet reg;
+	
+	public BD_Servicio(String bbdd){
+		super(bbdd);
+	}	
+	
+	public  Vector<Servicios> listadoServicios(String Servicios){
+		String cadenaSQL="SELECT * from servicios WHERE nombre='"+Servicios+"'";
+		Vector<Servicios> listaServicios=new Vector<Servicios>();
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			while ( reg.next()){
+				listaServicios.add(new Servicios(reg.getString("codigo"),reg.getString("nombre"),reg.getString("descripcion")));
+				
+			}
+			s.close();
+			this.cerrar();
+			return listaServicios;
+		}
+		catch ( SQLException e){		
+			return null;			
+		}
+	}
+	
+
 
 }
