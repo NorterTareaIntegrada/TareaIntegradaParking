ad8d6eba20cadcd8639148d4b9ddf6d0b56df4c9
admin <admin@Win7_1TB>
2017-05-23 14:44:19 +0200

    clases de plazas sin acabar

--- TareaIntegradaParking/src/Items/Plazas.java
+++ TareaIntegradaParking/src/Items/Plazas.java
@@ -1,98 +1,59 @@
 package Items;
 
+import java.time.LocalDateTime;
+
 public class Plazas {
-	private String codGaraje;
-	private String numPlaza;
-	private String numPlanta;
-	private double dimensiones;
-	private String abonado;
-	private String hora_entrada;
-	private String codTicket;
-	
-	
-	public Plazas(String codGaraje, String numPlaza, String numPlanta,
-			double dimensiones, String abonado, String hora_entrada,
-			String codTicket) {
+	private int codGaraje;
+	private int numPlaza;
+	private int numPlanta;
+	private int dimensiones;
+	private String matricula;
+	private LocalDateTime hora_entrada;
+	private int codTicket;
+	private Boolean activa;
+
+	public Plazas(int codGaraje, int numPlaza, int numPlanta, int dimensiones, String matricula,
+			LocalDateTime hora_entrada, int codTicket) {
 		super();
 		this.codGaraje = codGaraje;
 		this.numPlaza = numPlaza;
 		this.numPlanta = numPlanta;
 		this.dimensiones = dimensiones;
-		this.abonado = abonado;
+		this.matricula = matricula;
 		this.hora_entrada = hora_entrada;
 		this.codTicket = codTicket;
+		this.activa = true;
 	}
 
+	public Plazas() {
+		this.activa = false;
+	}
 
-	public String getCodGaraje() {
+	public int getCodGaraje() {
 		return codGaraje;
 	}
 
-
-	public void setCodGaraje(String codGaraje) {
-		this.codGaraje = codGaraje;
-	}
-
-
-	public String getNumPlaza() {
-		return numPlaza;
-	}
-
-
-	public void setNumPlaza(String numPlaza) {
-		this.numPlaza = numPlaza;
-	}
-
-
-	public String getNumPlanta() {
-		return numPlanta;
-	}
-
-
-	public void setNumPlanta(String numPlanta) {
-		this.numPlanta = numPlanta;
-	}
-
-
-	public double getDimensiones() {
-		return dimensiones;
-	}
-
-
-	public void setDimensiones(double dimensiones) {
-		this.dimensiones = dimensiones;
-	}
-
-
-	public String getAbonado() {
-		return abonado;
-	}
-
-
-	public void setAbonado(String abonado) {
-		this.abonado = abonado;
-	}
-
-
-	public String getHora_entrada() {
-		return hora_entrada;
-	}
-
-
-	public void setHora_entrada(String hora_entrada) {
-		this.hora_entrada = hora_entrada;
-	}
-
-
-	public String getCodTicket() {
+	public int getCodTicket() {
 		return codTicket;
 	}
 
-
-	public void setCodTicket(String codTicket) {
-		this.codTicket = codTicket;
+	public int getNumPlaza() {
+		return numPlaza;
 	}
-	
-	
-	
+
+	public int getNumPlanta() {
+		return numPlanta;
+	}
+
+	public int getDimensiones() {
+		return dimensiones;
+	}
+
+	public LocalDateTime getHora_entrada() {
+		return hora_entrada;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
 }
